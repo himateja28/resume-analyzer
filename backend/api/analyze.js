99b0051f6161d8dc5b49833e40ea9197f4f81dfd
MUSUNURI HIMATEJA <musunurihimateja@gmail.com>
2025-09-18 19:16:58 +0530

    Update analyze.js

--- backend/api/analyze.js
+++ backend/api/analyze.js
@@ -1,5 +1,16 @@
 // api/analyze.js
 const serverless = require('serverless-http');
 const app = require('../server'); // import the Express app
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                       // local dev
+    "https://resume-analyzer-rosy.vercel.app",     // your frontend domain
+    /\.vercel\.app$/                               // allow all vercel.app subdomains (mobile included)
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 module.exports = app;
 module.exports.handler = serverless(app);
