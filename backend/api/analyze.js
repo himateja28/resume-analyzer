e7caa410ebafc64c29da9ccf47ccf600c1e5b98f
MUSUNURI HIMATEJA <musunurihimateja@gmail.com>
2025-09-18 19:17:15 +0530

    Update analyze.js

--- backend/api/analyze.js
+++ backend/api/analyze.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors({
   origin: [
     "http://localhost:5173",                       // local dev
-    "https://resume-analyzer-rosy.vercel.app",     // your frontend domain
+    "https://resume-analyzer-gpsm.vercel.app/",     // your frontend domain
     /\.vercel\.app$/                               // allow all vercel.app subdomains (mobile included)
   ],
   methods: ["GET", "POST"],
